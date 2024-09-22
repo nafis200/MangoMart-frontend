f91c44e1a10de99c90795ea78e13d48ff6e2fd84
Rakesh Biswas <rbiswas01999@gmail.com>
2024-09-22 15:35:09 +0600

    login fixed

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -11,16 +11,67 @@ import { FcGoogle } from "react-icons/fc";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Swal from 'sweetalert2';
-// import { AuthContext } from '../providers/AuthProvider';
 import useAuth from '../../hooks/useAuth';
 import { Button, Container, TextField } from '@mui/material';
 import useAxiosPublic from '../../hooks/useAxiosPublic';
+import { AuthContext } from "../../providers/AuthProvider";
 
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const { googleSignIn } = useAuth();
+  const { signIn } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const axiosPublic = useAxiosPublic();
+
+  const from = location.state?.from?.pathname || "/";
+
+  const handleLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    signIn(email, password)
+      .then(result => {
+        Swal.fire({
+          title: "User Login Successful.",
+          showClass: {
+            popup: `animate__animated animate__fadeInUp animate__faster`
+          },
+          hideClass: {
+            popup: `animate__animated animate__fadeOutDown animate__faster`
+          }
+        });
+        navigate(from, { replace: true });
+      })
+      .catch(error => {
+        console.error(error);
+        toast.error('Invalid Email/Password');
+      });
+  }
+
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then(result => {
+        const userInfo = {
+          email: result.user?.email,
+          name: result.user?.displayName
+        }
+        axiosPublic.post('/users', userInfo)
+          .then(() => {
+            navigate('/');
+          });
+      });
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200 bg-[url('https://i.postimg.cc/Y075n05X/1000-F-668433624-HGKul-Uw-Qjae-LV8-Xay-QYy6-F3-RCVQff-TGv.jpg')] animate__animated animate__slideInLeft animate__delay-1s">
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+
       <div className="hero-content flex-col">
         <div className="text-center">
           <div className="text-3xl md:text-5xl font-bold text-black my-3">
@@ -29,61 +80,112 @@ const Login = () => {
             </h1>
           </div>
           <div className="card flex w-full lg:w-[600px] lg:h-[600px] md:w-[600px] md:h-[600px] shadow-2xl bg-base-100 mt-10 animate__animated animate__slideInLeft animate__delay-2">
-            <form className="card-body">
-              <div className="form-control animate__animated animate__slideInLeft animate__delay-5s">
-                <label className="label">
-                  <span className="label-text font-semibold text-[15px] md:text-[25px] mb-2">
-                    Email
-                  </span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered focus:ring focus:ring-red-500"
-                  name="email"
-                  required
-                />
-              </div>
-              <div className="form-control relative animate__animated animate__slideInLeft animate__delay-5s ">
-                <label className="label">
-                  <span className="label-text font-semibold mb-2 text-[15px] md:text-[25px] ">
-                    Password
-                  </span>
-                </label>
-                <input
-                  type={showPassword ? "text" : "password"}
-                  name="password"
-                  required
-                  placeholder="Password"
-                  className="input input-bordered focus:ring focus:ring-red-500"
-                />
-                <span
-                  className="absolute  bottom-11 right-2"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? (
-                    <FaEye className="text-2xl"></FaEye>
-                  ) : (
-                    <FaEyeSlash className="text-2xl"></FaEyeSlash>
-                  )}
-                </span>
-                <label className="label">
-                  <a
-                    href="#"
-                    className="label-text-alt link link-hover font-bold text-[] md:text-[15px] animate__animated animate__slideInLeft"
+            <Container maxWidth="sm" className="relative z-10">
+              <div className="flex items-center justify-center min-h-screen">
+                <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+                  <h1 className="text-3xl md:text-5xl font-bold text-orange-600 mb-6 text-center animate__animated animate__fadeInDown">
+                    Login now!
+                  </h1>
+
+                  <form onSubmit={handleLogin}>
+                    <div className="mb-4">
+                      <TextField
+                        label="Email"
+                        type="email"
+                        name="email"
+                        placeholder="email"
+                        required
+                        fullWidth
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <TextField
+                        label="Password"
+                        type="password"
+                        name="password"
+                        required
+                        placeholder="password"
+                        fullWidth
+                      />
+                      <div className="text-right">
+                        <Link to="#" className="text-orange-500 text-sm">
+                          Forgot password?
+                        </Link>
+                      </div>
+                    </div>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      fullWidth
+                      className="h-11 mb-4"
+                      sx={{
+                        backgroundColor: 'orange', // Sets the background color to orange
+                        '&:hover': {
+                          backgroundColor: 'darkorange', // Sets the hover color to a darker shade of orange
+                        },
+                      }}
+                    >
+                      <span className="text-lg text-black">Login</span>
+                    </Button>
+                  </form>
+
+                  <p className="text-center text-lg mt-4">
+                    New to this site? Please{' '}
+                    <Link to="/register" className="text-orange-600 font-bold">
+                      Register
+                    </Link>
+                  </p>
+
+                  <div className="text-center mb-4">
+                    <span className="text-lg font-semibold">or</span>
+                  </div>
+
+                  {/* <div>
+                        <Button
+                            onClick={handleGoogleSignIn}
+                            variant="outlined"
+                            fullWidth
+                            
+                            startIcon={<FcGoogle />}
+                            className="mb-4"
+                        >
+                            Continue with Google
+                        </Button>
+                    </div> */}
+
+                  <Button
+                    onClick={handleGoogleSignIn}
+                    variant="outlined"
+                    fullWidth
+                    startIcon={<FcGoogle />}
+                    className="mb-4"
+                    sx={{
+                      color: 'orange', // Set text color to orange
+                      borderColor: 'orange', // Set border color to orange
+                      '&:hover': {
+                        borderColor: 'darkorange', // Change border color on hover
+                        color: 'darkorange', // Change text color on hover
+                      },
+                    }}
                   >
-                    Forgot password?
-                  </a>
-                </label>
+                    Continue with Google
+                  </Button>
+
+
+                  <p className="text-center text-lg">
+                    Go back to{' '}
+                    <Link to="/" className="text-orange-600 font-bold">
+                      Home
+                    </Link>
+                  </p>
+
+                </div>
               </div>
-              <div className="form-control mt-6 animate__animated animate__slideInLeft animate__delay-5s">
-                <button className="btn btn-primary">Sign In</button>
-              </div>
-              <div className="flex justify-end text-red-600 underline"> </div>
-            </form>
+            </Container>
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -44,7 +44,7 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             // setLoading(false);
-            // console.log('current user', currentUser);
+            console.log('current user', currentUser);
             if(currentUser){
                 const userInfo = {email: currentUser.email};
                 axiosPublic.post('/jwt', userInfo)
